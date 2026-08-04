af43d5121cb3a97b6e3840c412e2e48db60f1f84
cherrblyria <cherrblyria@gmail.com>
2026-08-04 20:43:15 +0700

    u

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -8,13 +8,15 @@
 let
   myCursorTheme = pkgs.runCommand "my-cursor-theme" { } ''
     mkdir -p $out/share/icons/mizuki-psekai-cursor
-    cp -r ${../../assets/cursors/mizuki-psekai-cursor}/* $out/share/icons/mizuki-psekai-cursor/
+    cp -r ${./assets/cursors/mizuki-psekai-cursor}/* $out/share/icons/mizuki-psekai-cursor/
   '';
 in
 {
   imports = [
     ./hardware-configuration.nix
     inputs.noctalia-greeter.nixosModules.default
+
+    ./modules/syncthing.nix
   ];
 
   networking.hostName = "nixos";
@@ -59,72 +61,42 @@ in
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
 
-  # Syncthings
-  services.syncthing = {
-    enable = true;
-    openDefaultPorts = true;
-    user = "cherr";
-    group = "users";
-    dataDir = "/home/cherr";
-    settings = {
-      gui.user = "cherr";
-      devices = {
-        "Phone" = {
-          id = "O76SLWB-LZJFWNX-XY3CRJV-SG7PIMM-P3HNDPU-XZYU7HR-736KX5Q-QGZ45QV";
-        };
-        "Tablet" = {
-          id = "7ZYRPOK-AJENNVR-JJFQG2A-NKX4HPY-MJNQQMU-3ADI5DV-DBIMFAY-ARZ3GAT";
-        };
-      };
-      folders = {
-        "vault" = {
-          id = "vault";
-          label = "Cherr's Sanctuary";
-          path = "/home/cherr/Documents/Cherr's Sanctuary";
-          ignorePatterns = [
-            ".git/"
-            ".git"
-            ".gitignore"
-            ".obsidian/community-plugins.json"
-          ];
-          devices = [
-            "Phone"
-            "Tablet"
-          ];
-        };
-        "shared-dcim" = {
-          id = "shared-dcim";
-          label = "DCIM";
-          path = "/home/cherr/DCIM";
-          devices = [
-            "Phone"
-            "Tablet"
-          ];
-        };
-        "shared-pics" = {
-          id = "shared-pics";
-          label = "Pictures";
-          path = "/home/cherr/Pictures";
-          devices = [
-            "Phone"
-            "Tablet"
-          ];
-        };
+  # Install custom local font files system-wide
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      adwaita-fonts
+      nerd-fonts.caskaydia-cove
+
+      (pkgs.runCommand "my-custom-fonts" { } ''
+        mkdir -p $out/share/fonts/truetype
+        cp -r ${./assets/fonts}/* $out/share/fonts/truetype/
+      '')
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Adwaita Sans"
+          "Noto Serif CJK JP"
+          "Google Sans"
+        ];
+        sansSerif = [
+          "Adwaita Sans"
+          "Noto Sans CJK JP"
+          "Google Sans"
+        ];
+        monospace = [
+          "Caskaydia Cove Nerd Font"
+          "Noto Sans Mono CJK JP"
+          "Google Sans"
+        ];
+        emoji = [ "Noto Color Emoji" ];
       };
     };
   };
-
-  # Install custom local font files system-wide
-  fonts.packages = with pkgs; [
-    adwaita-fonts
-    noto-fonts
-    noto-fonts-cjk-sans
-    nerd-fonts.caskaydia-cove
-    (pkgs.runCommand "my-custom-fonts" { } ''
-      mkdir -p $out/share/fonts/truetype
-      cp -r ${../../assets/fonts}/* $out/share/fonts/truetype/
-    '')
-  ];
 
   # Apply GTK / Pointer defaults system-wide
   environment.variables = {
---
+++ hosts/nixos/modules/syncthing.nix
@@ -0,0 +1,55 @@
+{
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = "cherr";
+    group = "users";
+    dataDir = "/home/cherr";
+    settings = {
+      gui.user = "cherr";
+      devices = {
+        "Phone" = {
+          id = "O76SLWB-LZJFWNX-XY3CRJV-SG7PIMM-P3HNDPU-XZYU7HR-736KX5Q-QGZ45QV";
+        };
+        "Tablet" = {
+          id = "7ZYRPOK-AJENNVR-JJFQG2A-NKX4HPY-MJNQQMU-3ADI5DV-DBIMFAY-ARZ3GAT";
+        };
+      };
+      folders = {
+        "vault" = {
+          id = "vault";
+          label = "Cherr's Sanctuary";
+          path = "/home/cherr/Documents/Cherr's Sanctuary";
+          ignorePatterns = [
+            ".git/"
+            ".git"
+            ".gitignore"
+            ".obsidian/community-plugins.json"
+          ];
+          devices = [
+            "Phone"
+            "Tablet"
+          ];
+        };
+        "shared-dcim" = {
+          id = "shared-dcim";
+          label = "DCIM";
+          path = "/home/cherr/DCIM";
+          devices = [
+            "Phone"
+            "Tablet"
+          ];
+        };
+        "shared-pics" = {
+          id = "shared-pics";
+          label = "Pictures";
+          path = "/home/cherr/Pictures";
+          devices = [
+            "Phone"
+            "Tablet"
+          ];
+        };
+      };
+    };
+  };
+}
