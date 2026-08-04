5a97264e7c84ab8c1ed29405a1ffc84d0a14dadb
cherrblyria <cherrblyria@gmail.com>
2026-08-04 21:53:19 +0700

    c

--- hosts/nixos/modules/core/boot.nix
+++ hosts/nixos/modules/core/boot.nix
@@ -27,7 +27,7 @@
 
   # Secrets
   sops = {
-    defaultSopsFile = ./secrets/secrets.yaml;
+    defaultSopsFile = ../../secrets/secrets.yaml;
     age.keyFile = "/var/lib/sops-nix/key.txt";
     secrets."tailscale_key".owner = "cherr";
   };
