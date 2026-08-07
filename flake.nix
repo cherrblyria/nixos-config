579db9992997a77a994c3c512090962437a8f4cd
cherrblyria <cherrblyria@gmail.com>
2026-08-08 00:38:43 +0700

    c

--- flake.nix
+++ flake.nix
@@ -26,8 +26,17 @@
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
-          ./configuration.nix
+          inputs.home-manager.nixosModules.home-manager
+          inputs.noctalia-greeter.nixosModules.default
+          inputs.sops-nix.nixosModules.sops
+          inputs.spicetify-nix.nixosModules.spicetify
+
           ./hardware-configuration.nix
+
+          ./modules/core
+          ./modules/home
+          ./modules/packages
+          ./modules/services
         ];
       };
     };
