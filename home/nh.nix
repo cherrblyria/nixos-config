0c55ec86482498e609b35425354fc9626ba34335
cherrblyria <cherrblyria@gmail.com>
2026-08-08 11:25:12 +0700

    c

--- home/nh.nix
+++ home/nh.nix
@@ -2,5 +2,10 @@
   programs.nh = {
     enable = true;
     flake = "/home/cherr/nixos-config";
+    clean = {
+      enable = true;
+      dates = "weekly";
+      extraArgs = "--keep 5 --keep-since 3d";
+    };
   };
 }
