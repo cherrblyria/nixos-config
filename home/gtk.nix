6d1e1bb30d4d8e97db36ef13dc911f7d17af9633
cherrblyria <cherrblyria@gmail.com>
2026-08-09 16:44:58 +0700

    f

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,22 +1,32 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  home.pointerCursor = {
-    enable = true;
-    gtk.enable = true;
-    x11.enable = true;
-    name = "mizuki-psekai-cursor";
-    package = pkgs.mizuki-psekai-cursor;
-    size = 24;
+  home = {
+    # Papirus Icons
+    activation.installPapirus = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ ! -d "$HOME/.local/share/icons/Papirus" ]; then
+        run cp -r ${pkgs.papirus-icon-theme}/share/icons/Papirus* $HOME/.local/share/icons/
+        run chmod -R u+w $HOME/.local/share/icons/Papirus*
+      fi
+    '';
+
+    # Cursor
+    pointerCursor = {
+      enable = true;
+      gtk.enable = true;
+      x11.enable = true;
+      name = "mizuki-psekai-cursor";
+      package = pkgs.mizuki-psekai-cursor;
+      size = 24;
+    };
   };
+
+  # GTK
   gtk = {
     enable = true;
     theme = {
       name = "adw-gtk3";
       package = pkgs.adw-gtk3;
     };
-    iconTheme = {
-      name = "Papirus";
-      package = pkgs.papirus-icon-theme;
-    };
+    iconTheme.name = "Papirus";
   };
 }
