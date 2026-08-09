11751dd60d8d7ace785448c93d07581e5ce8d5f5
cherrblyria <cherrblyria@gmail.com>
2026-08-09 21:34:22 +0700

    f

--- modules/packages/overlays.nix
+++ modules/packages/overlays.nix
@@ -3,7 +3,7 @@
     (final: prev: {
       mizuki-psekai-cursor = prev.runCommand "mizuki-psekai-cursor" { } ''
         mkdir -p $out/share/icons/mizuki-psekai-cursor
-        cp -r ${../assets/cursors/mizuki-psekai-cursor}/* $out/share/icons/mizuki-psekai-cursor/
+        cp -r ${../../assets/cursors/mizuki-psekai-cursor}/* $out/share/icons/mizuki-psekai-cursor/
       '';
     })
   ];
