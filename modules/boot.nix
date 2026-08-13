7de0f4edab1d5a01967d5f217907b77a39aae75a
Cherrblyria <cherrblyria@gmail.com>
2026-08-13 12:17:02 +0700

    Update boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -15,6 +15,17 @@
     "nix-command"
   ];
 
+  nix.settings = {
+    extra-substituters = [ 
+      "https://noctalia.cachix.org"
+      "https://nix-gaming.cachix.org"
+    ];
+    extra-trusted-public-keys = [ 
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
+  };
+
   # Enable 32bit for osu!
   hardware.graphics = {
     enable = true;
