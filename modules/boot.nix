509e6fd1115150926076417cf5d6b791179d0894
Cherrblyria <cherrblyria@gmail.com>
2026-08-13 11:08:15 +0700

    Update boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -12,22 +12,10 @@
     loader.efi.canTouchEfiVariables = true;
   };
 
-  nix.settings = {
-    substituters = [ 
-      "https://noctalia.cachix.org"
-      "https://nix-gaming.cachix.org"
-    ];
-    trusted-public-keys = [ 
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    ];
-
-
-    experimental-features = [
+  nix.settings.experimental-features = [
       "flakes"
       "nix-command"
     ];
-  };
 
   # Enable 32bit for osu!
   hardware.graphics = {
