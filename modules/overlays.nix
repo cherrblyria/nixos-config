c075d5d02915cc93549df5aaf17b7ce9d777cbcf
Cherrblyria <cherrblyria@gmail.com>
2026-08-13 08:20:47 +0700

    Create overlays.nix

---
+++ modules/overlays.nix
@@ -0,0 +1,10 @@
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+    mizuki-psekai-cursor = prev.runCommand "mizuki-psekai-cursor" { } ''
+      mkdir -p $out/share/icons/mizuki-psekai-cursor
+      cp -r ${../assets/cursors/mizuki-psekai-cursor}/* $out/share/icons/mizuki-psekai-cursor/
+    '';
+    })
+  ];
+}
