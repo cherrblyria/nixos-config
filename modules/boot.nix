1d8745533993b61cdeae2b54b73aa5893342fd19
Cherrblyria <cherrblyria@gmail.com>
2026-08-13 09:31:00 +0700

    Update boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -13,11 +13,11 @@
   };
 
   nix.settings = {
-    extra-substituters = [ 
+    substituters = [ 
       "https://noctalia.cachix.org"
       "https://nix-gaming.cachix.org"
     ];
-    extra-trusted-public-keys = [ 
+    trusted-public-keys = [ 
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
