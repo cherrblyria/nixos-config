dce016198df3e1a15c354c491e4f9b4784a6e3a2
Cherrblyria <cherrblyria@gmail.com>
2026-08-13 08:17:11 +0700

    Update boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -4,17 +4,7 @@
   system.stateVersion = "26.11";
 
   # Overlays
-  nixpkgs = {
-    config.allowUnfree = true;
-    overlays = [
-      (final: prev: {
-        mizuki-psekai-cursor = prev.runCommand "mizuki-psekai-cursor" { } ''
-          mkdir -p $out/share/icons/mizuki-psekai-cursor
-          cp -r ${../assets/cursors/mizuki-psekai-cursor}/* $out/share/icons/mizuki-psekai-cursor/
-        '';
-      })
-    ];
-  };
+  nixpkgs.config.allowUnfree = true;
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
