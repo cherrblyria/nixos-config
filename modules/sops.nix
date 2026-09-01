3a65bc33ababf52cc907d67197ed9b68fab92f29
cherrblyria <cherrblyria@gmail.com>
2026-09-01 17:36:49 +0700

    feat(secrets): wallhaven api

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,7 +1,20 @@
+{ config, ... }:
 {
   sops = {
     defaultSopsFile = ../secrets/secrets.yaml;
     age.keyFile = "/var/lib/sops-nix/key.txt";
-    secrets."tailscale_key".owner = "cherr";
+    secrets = {
+      "tailscale_key".owner = "cherr";
+      "wallhaven_api_key".owner = "cherr";
+    };
+
+    templates."noctalia_wallhaven.toml" = {
+      owner = "cherr";
+      path = "${config.users.users.cherr.home}/.config/noctalia/config.toml";
+      content = ''
+        [plugin_settings."noctalia/wallhaven"]
+        api_key = "${config.sops.placeholder.wallhaven_api_key}"
+      '';
+    };
   };
 }
